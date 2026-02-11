fce4c8a0a560c67f48e5be3eeba6591b72fb3374
DrilonKajtazi <drilonkajtazi10@gmail.com>
2026-02-11 20:59:33 +0100

    INIT project

---
+++ Messaging-API/src/test/java/org/messaging/messagingapi/MessagingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.messaging.messagingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Messaging-Core/src/test/java/org/messaging/messagingcore/MessagingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.messaging.messagingcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
